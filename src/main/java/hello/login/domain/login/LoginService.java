e61c5d69920c9a0ab5cf5113fadb0391701103ee
ChoiJungHwan <vkdlxj3562@naver.com>
2021-08-24 15:37:39 +0900

    로그인 기능 구현

---
+++ src/main/java/hello/login/domain/login/LoginService.java
@@ -0,0 +1,26 @@
+package hello.login.domain.login;
+
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @return null 로그인 실패
+     */
+    public Member login(String LoginId, String password) {
+        return memberRepository.findByLoginId(LoginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+}
